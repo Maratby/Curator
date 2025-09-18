7639f5168ada58e312edfe2c96cedaceec225f92
Marie <maratby@outlook.com>
2025-09-18 22:05:28 +0100

    fixed the mod not working

--- items/Boosters.lua
+++ items/Boosters.lua
@@ -4,10 +4,11 @@ SMODS.Booster {
     key = 'curated_regular',
     name = 'Curated Pack',
     atlas = "curated-1st",
+    diiscovered = true,
     pos = { x = 0, y = 0 },
     cost = 6,
     ---The placeholder "RandomMod" value will be used as a flag for backup later
-    config = { extra = 4, choose = 1, curated = { mod_name = "RandomMod", } },
+    config = { extra = 4, choose = 1, curated = { mod_name = "RandomMod", mod_id = "randommod"} },
     group_key = 'k_curated_pack',
     loc_vars = function(self, info_queue, card)
         return { vars = { self.config.choose, self.config.extra, self.config.curated.mod_name } }
@@ -17,11 +18,12 @@ SMODS.Booster {
             local temp = pseudorandom_element(Curator.joker_table, pseudoseed('curated_pack'))
             if temp then
                 self.config.curated.mod_name = temp.DisplayName
+                self.config.curated.mod_id = temp.NormalisedName
             end
         end
     end,
     create_card = function(self, card, i)
-        local tempset = "cura_curated_pack_pool_" .. self.config.curated.mod_name
+        local tempset = "cura_curated_pack_pool_" .. self.config.curated.mod_id
         local newCard = SMODS.create_card { set = tempset, area = G.pack_cards }
         return newCard
     end,
@@ -33,6 +35,7 @@ SMODS.Booster {
     key = 'curated_jumbo',
     name = 'Curated Pack',
     atlas = "curated-1st",
+    diiscovered = true,
     pos = { x = 1, y = 0 },
     cost = 10,
     config = { extra = 5, choose = 1, curated = { mod_name = "RandomMod", } },
@@ -45,11 +48,12 @@ SMODS.Booster {
             local temp = pseudorandom_element(Curator.joker_table, pseudoseed('curated_pack'))
             if temp then
                 self.config.curated.mod_name = temp.DisplayName
+                self.config.curated.mod_id = temp.NormalisedName
             end
         end
     end,
     create_card = function(self, card, i)
-        local tempset = "cura_curated_pack_pool_" .. self.config.curated.mod_name
+        local tempset = "cura_curated_pack_pool_" .. self.config.curated.mod_id
         local newCard = SMODS.create_card { set = tempset, area = G.pack_cards }
         return newCard
     end,
@@ -61,6 +65,7 @@ SMODS.Booster {
     key = 'curated_mega',
     name = 'Curated Pack',
     atlas = "curated-1st",
+    diiscovered = true,
     pos = { x = 2, y = 0 },
     cost = 16,
     config = { extra = 7, choose = 2, curated = { mod_name = "RandomMod", } },
@@ -73,11 +78,12 @@ SMODS.Booster {
             local temp = pseudorandom_element(Curator.joker_table, pseudoseed('curated_pack'))
             if temp then
                 self.config.curated.mod_name = temp.DisplayName
+                self.config.curated.mod_id = temp.NormalisedName
             end
         end
     end,
     create_card = function(self, card, i)
-        local tempset = "cura_curated_pack_pool_" .. self.config.curated.mod_name
+        local tempset = "cura_curated_pack_pool_" .. self.config.curated.mod_id
         local newCard = SMODS.create_card { set = tempset, area = G.pack_cards }
         return newCard
     end,
@@ -91,6 +97,7 @@ SMODS.Booster {
     key = 'curated_selector',
     name = 'Curated Pack',
     atlas = "curated-2nd",
+    diiscovered = true,
     pos = { x = 0, y = 0 },
     cost = 16,
     config = { extra = 6, choose = 2, curated = { mod_name = "RandomMod", } },
